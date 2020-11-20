5ce9b2f16982f8de59423c5252effaaba324a8cc
L4v <ivos.jovan@protonmail.ch>
2020-11-20 23:10:29 +0100

    :ambulance: Critical hotfix for psw

--- src/main/java/com/schnabel/schnabel/SchnabelApplication.java
+++ src/main/java/com/schnabel/schnabel/SchnabelApplication.java
@@ -14,5 +14,18 @@ public class SchnabelApplication {
           .singletonMap("server.port", "8082"));
         app.run(args);
 	}
+	
+	@Bean
+	public CorsFilter corsFilter() {
+	    final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+	    final CorsConfiguration config = new CorsConfiguration();
+	    config.setAllowCredentials(true);
+	    // Don't do this in production, use a proper list  of allowed origins
+	    config.setAllowedOrigins(Collections.singletonList("*"));
+	    config.setAllowedHeaders(Arrays.asList("Origin", "Content-Type", "Accept"));
+	    config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "OPTIONS", "DELETE", "PATCH"));
+	    source.registerCorsConfiguration("/**", config);
+	    return new CorsFilter(source);
+	}
 
 }
