1c0601367e47179fb5907dc5abc1f92d49ee2629
L4v <ivosevic.jovan@uns.ac.rs>
2020-11-21 01:18:53 +0100

    :ambulance: Hotfix cors

--- src/main/java/com/schnabel/schnabel/SchnabelApplication.java
+++ src/main/java/com/schnabel/schnabel/SchnabelApplication.java
@@ -1,8 +1,13 @@
 package com.schnabel.schnabel;
 
+import java.util.Arrays;
 import java.util.Collections;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 @SpringBootApplication
 public class SchnabelApplication {
