899e7d9c41063f16bd5a778bbd138127858d2557
L4v <ivosevic.jovan@uns.ac.rs>
2020-11-21 02:37:10 +0100

    :ambulance: Another hotfix

--- src/main/java/com/schnabel/schnabel/pswregistration/RegistrationController.java
+++ src/main/java/com/schnabel/schnabel/pswregistration/RegistrationController.java
@@ -3,6 +3,7 @@ package com.schnabel.schnabel.pswregistration;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -16,6 +17,7 @@ public class RegistrationController {
     @Autowired
     private IHospitalService hospitalService;
 
+    @CrossOrigin(origins = "*", allowedHeaders = "*")
     @GetMapping("/pswapi/{api}")
     ResponseEntity<String> getByAPIKey(@PathVariable String api)
     {
