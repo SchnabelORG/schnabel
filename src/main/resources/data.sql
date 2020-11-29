07a0f74841eb0910578b1207724fd75edc1030dc
radovan-zupunski <rzupunski@gmail.com>
2020-11-29 22:22:21 +0100

    :card_file_box: fix table specialOffers

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -2,8 +2,8 @@ INSERT INTO hospitals("name", api_key) VALUES('sacred_heart_hospital', 'apishh12
 INSERT INTO hospitals("name", api_key) VALUES('princeton_plainsboro', 'apipp1234');
 INSERT INTO hospitals("name", api_key) VALUES('limanska_ambulanta', 'apila1234');
 
-INSERT INTO specialoffers(id, "name", content, paharmacyId) VALUES(1, 'Aspirin sale!', 'Aspirin sale, 50% off, buy now!', 'Jankovic');
-INSERT INTO specialoffers(id, "name", content, paharmacyId) VALUES(2, 'Loyalty card', '10% discount  with loyalty card!', 'Liman');
-INSERT INTO specialoffers(id, "name", content, paharmacyId) VALUES(3, 'Suplements', '30% of for all suplements', 'Liman');
-INSERT INTO specialoffers(id, "name", content, paharmacyId) VALUES(4, 'Special gift', 'Special gift for spent over 30e', 'Jankovic');
+INSERT INTO specialoffers(id, "name", content, pharmacyId) VALUES(1, 'Aspirin sale!', 'Aspirin sale, 50% off, buy now!', 'Jankovic');
+INSERT INTO specialoffers(id, "name", content, pharmacyId) VALUES(2, 'Loyalty card', '10% discount  with loyalty card!', 'Liman');
+INSERT INTO specialoffers(id, "name", content, pharmacyId) VALUES(3, 'Suplements', '30% of for all suplements', 'Liman');
+INSERT INTO specialoffers(id, "name", content, pharmacyId) VALUES(4, 'Special gift', 'Special gift for spent over 30e', 'Jankovic');
 
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -2,4 +2,4 @@ DROP TABLE IF EXISTS hospitals;
 CREATE TABLE hospitals(api_key VARCHAR(255) PRIMARY KEY, "name" VARCHAR(255));
 
 DROP TABLE IF EXISTS specialoffers;
-CREATE TABLE specialoffers(id SERIAL PRIMARY KEY, "name" VARCHAR(255), content VARCHAR(255));
+CREATE TABLE specialoffers(id SERIAL PRIMARY KEY, "name" VARCHAR(255), content VARCHAR(255), pharmacyId VARCHAR(255));
