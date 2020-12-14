8b596b36f3f84bae596179e177caad7d33245eb0
Marko Pekez <marko.ppekez25@gmail.com>
2020-12-14 23:07:56 +0100

    Psw http check med (#11)
    * :drug: Added get drug by name method
    
    * :ambulance: Fixed method in drug controller

--- src/main/java/com/schnabel/schnabel/drugs/controller/DrugController.java
+++ src/main/java/com/schnabel/schnabel/drugs/controller/DrugController.java
@@ -10,6 +10,7 @@ import org.apache.catalina.connector.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -35,6 +36,7 @@ public class DrugController
             : ResponseEntity.ok(drug);
     }
 
+    @CrossOrigin(origins = "*", allowedHeaders = "*")
     @GetMapping("/pswapi/drugs/name/{name}")
     public ResponseEntity<String> getById(@PathVariable String name)
     {
