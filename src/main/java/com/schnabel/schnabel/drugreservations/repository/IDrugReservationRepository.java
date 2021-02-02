26dee307c3c73b639623bfcdc91711335aafc65f
L4v <ivosevic.jovan@uns.ac.rs>
2021-02-02 16:22:31 +0100

    Added basic drug reservation backend (#25)
    * :sparkles: Added DrugReservation
    
    * :card_file_box: Added DrugReservation table sql commands.
    
    * :sparkles: Added isValid method to DrugReservation.
    
    * :white_check_mark: Added DrugReservation test
    
    * :bug: Added missing CASCADE in schema.sql
    
    * :memo: Added documentation to DrugReservation feature

---
+++ src/main/java/com/schnabel/schnabel/drugreservations/repository/IDrugReservationRepository.java
@@ -0,0 +1,13 @@
+package com.schnabel.schnabel.drugreservations.repository;
+
+import com.schnabel.schnabel.drugreservations.model.DrugReservation;
+
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ * Drug reservation repository interface
+ */
+public interface IDrugReservationRepository extends CrudRepository<DrugReservation, Integer>
+{
+    
+}
