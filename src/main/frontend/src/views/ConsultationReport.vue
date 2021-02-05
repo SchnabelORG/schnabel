ca8d06fb4e63343e02fa56e6e5575244f0b4f7f5
radovan-zupunski <rzupunski@gmail.com>
2021-02-05 16:12:49 +0100

    :lipstick: updated ui for pharmacist consultation

--- src/main/frontend/src/views/ConsultationReport.vue
+++ src/main/frontend/src/views/ConsultationReport.vue
@@ -44,10 +44,34 @@
             <v-stepper-items>
                 <v-stepper-content step="1">
                     <v-card
+                        elevation="3"
                         class="mb-12"
-                        color="grey lighten-1"
-                        height="200px"
-                    ></v-card>
+                        height="200px">
+
+                        <v-text-field
+                            v-model="patientName"
+                            label="Search patient">
+                        </v-text-field>
+
+                        <table style="width:100%">
+                            <tr>
+                                <td><b>Name:</b> </td>
+                                <td>Radovan</td>
+                            </tr>
+                            <tr>
+                                <td><b> Surname:</b></td>
+                                <td>Zupunski</td>
+                            </tr>
+                            <tr>
+                                <td><b>Email:</b></td>
+                                <td>radovan.zupunski@gmail.com</td>
+                            </tr>
+                            <tr>
+                                <td><b>Address:</b></td>
+                                <td>Slobodana Bajica 17, Novi Sad</td>
+                            </tr>
+                        </table>         
+                    </v-card>
 
                     <v-row>
                         <v-col class="text-left">
@@ -62,11 +86,13 @@
                 </v-stepper-content>
         
                 <v-stepper-content step="2">
-                    <v-card
-                        class="mb-12"
-                        color="grey lighten-1"
-                        height="200px"></v-card>
                     
+                    <v-textarea
+                            filled
+                            shaped
+                            label="Consultation info">
+                    </v-textarea>    
+
                     <v-row>
                         <v-col class="text-left">
                             <v-btn
@@ -92,8 +118,40 @@
                 <v-stepper-content step="3">
                     <v-card
                         class="mb-12"
-                        color="grey lighten-1"
-                        height="200px"></v-card>
+                        
+                        height="200px">
+                        <v-row>
+                            <div id="select-medication">
+                                <v-select
+                                    :items="medications"
+                                    label="Medication name"
+                                    :rules="[rules.required]"
+                                    :item-disabled="checkIfDisabled">
+                                    <template slot="selection" slot-scope="data" >
+                                        {{ data.item.name }}  {{ data.item.dosage }}
+                                    </template>
+                                    <template slot="item" slot-scope="data">
+                                        {{ data.item.name }}  {{ data.item.dosage }}
+                                    </template>
+                                </v-select>
+                            </div>
+                            <v-btn id="add-med-btn" elevation="2" @click="addMedication" class="deep-orange white--text">
+                                Add
+                            </v-btn>
+                        </v-row>
+                        
+                        <v-data-table :items="prescripedMedication"
+                                        :hide-default-footer="true"
+                                        :hide-default-header="true">
+                            <template v-slot:item="row">
+                                <tr>
+                                    <td>{{row.item.medicationName}}</td>
+                                    <td>{{row.item.medicationDosage}}</td>
+                                    <td>x{{row.item.medicationQuantity}}</td>
+                                </tr>
+                            </template>
+                        </v-data-table>
+                    </v-card>
             
                     <v-row>
                         <v-col class="text-left">
@@ -182,15 +240,40 @@
         data() {
             return {
                 e1: 1,
+                patientName: '',
+                medicationName: '',
+                medications: [],
+                prescripedMedication: [],
+                rules: {
+                    required: value => !!value || 'Required.',
+                    min: v => v.length >= 8 || 'Min 8 characters',
+                    isNmb: v => /^\d+$/.test(v) || 'Must be a number',
+                },
+
             }
         },
         methods:{
             getAllPatients: function(){
                 console.log("aa");
+            },
+            getAllMedications: function(){
+                this.medications.push({name: 'Brufen', dosage: '100mg', company: 'Hemofarm'});
+                this.medications.push({name: 'Aspirin', dosage: '200mg', company: 'Hemofarm'});
+                this.medications.push({name: 'Morfijum', dosage: '300mg', company: 'Hemofarm'});
+                this.medications.push({name: 'Bensedin', dosage: '400mg', company: 'Hemofarm'});
+                this.medications.push({name: 'Amigren', dosage: '1000mg', company: 'Hemofarm'});
+            },
+            checkIfDisabled: function(item){
+                if(item.name === 'Morfijum')
+                    return true;
+                else
+                    return false;
+
             }
         },
         mounted(){
             this.getAllPatients();
+            this.getAllMedications();
         },
     }
 </script>
@@ -201,5 +284,14 @@
         grid-template-columns:auto;
         place-items: center;
         min-height: 100%;
+    }
+    #add-med-btn{
+        margin: 0;
+        margin-top: 1.5%;
+        width: 10%;
+    }
+    #select-medication{
+        margin-left: 2%;
+        width: 85%;
     }
 </style>
