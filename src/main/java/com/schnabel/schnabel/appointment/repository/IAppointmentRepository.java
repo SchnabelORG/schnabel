b9f157d8b0e5832703de9c9f0880521810beb485
Kris98Wolf <kristian98farkas@gmail.com>
2021-05-07 13:03:10 +0200

    :art: Added shift and started appointment

---
+++ src/main/java/com/schnabel/schnabel/appointment/repository/IAppointmentRepository.java
@@ -0,0 +1,15 @@
+package com.schnabel.schnabel.appointment.repository;
+
+import com.schnabel.schnabel.appointment.model.Appointment;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+
+/**
+ * Appointment Jpa repository interface
+ */
+@RepositoryRestResource(collectionResourceRel = "appointments", path = "appointment")
+public interface IAppointmentRepository extends JpaRepository<Appointment, Long>
+{
+}
